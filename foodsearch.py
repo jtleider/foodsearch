32945b2a3a23ba455a7bdc4d2a05d97b8c3da46d
jtleider <jtleider@gmail.com>
2017-08-26 14:49:35 -0500

    Create search function

--- foodsearch.py
+++ foodsearch.py
@@ -1,18 +1,42 @@
-# IPython log file
-
 import googlemaps
 import csv
+from collections import defaultdict
 
-cli = googlemaps.Client(key=open('apikey').read())
-nearby = cli.places_nearby(location=(40.748920, -73.968912),
-	keyword='supermarket',
-	rank_by='distance')
-placeids = [r['place_id'] for r in nearby['results']]
+def search(output, location, search_term, api_key):
+	cli = googlemaps.Client(key=api_key)
+	csv_writer = csv.writer(output)
+	csv_writer.writerow(location)
+	csv_writer.writerow(['Search Term', 'Store Name', 'Address', 'Phone Number'])
+	next_page_token = None
+	while True:
+		print("Searching...")
+		nearby = cli.places_nearby(location=location,
+			keyword=search_term,
+			rank_by='distance',
+			page_token = next_page_token)
+		try:
+			assert nearby['status'] == 'OK'
+		except AssertionError:
+			print('Warning, status not OK: {0}'.format(nearby['status']))
+			print('Continuing anyway...')
+		placeids = [r['place_id'] for r in nearby['results']]
+		for placeid in placeids:
+			details = cli.place(place_id=placeid)
+			try:
+				assert details['status'] == 'OK'
+			except AssertionError:
+				print('Warning, status not OK: {0}'.format(details['status']))
+				print('Continuing anyway...')
+			details = defaultdict(str, details['result'])
+			csv_writer.writerow((search_term, details['name'], details['formatted_address'], details['formatted_phone_number']))
+		if 'next_page_token' in nearby.keys():
+			next_page_token = nearby['next_page_token']
+		else:
+			break
+
+
 f = open('temp.csv', 'w')
-csv_writer = csv.writer(f)
-for placeid in placeids:
-	details = cli.place(place_id=placeid)
-	details = details['result']
-	csv_writer.writerow((details['name'], details['formatted_address'], details['formatted_phone_number']))
+for search_term in ['supermarket', 'convenience store', 'dollar store']:
+	search(f, (40.748920, -73.968912), search_term, open('apikey').read())
 f.close()
 
